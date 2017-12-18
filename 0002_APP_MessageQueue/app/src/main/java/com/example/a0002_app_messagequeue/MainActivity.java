195be2709b10702c08efdea5b9d093b42c0f076a
li_heqing <443733701@qq.com>
2017-12-18 22:04:19 +0800

    [Description]：使用不同方法创建两个子线程 [Author]：liheqingSky [Date]:  2017年12月18日 22:05:26
     On branch master
     Your branch is ahead of 'origin/master' by 1 commit.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	modified:   0002_APP_MessageQueue/app/src/main/java/com/example/a0002_app_messagequeue/MainActivity.java

--- 0002_APP_MessageQueue/app/src/main/java/com/example/a0002_app_messagequeue/MainActivity.java
+++ 0002_APP_MessageQueue/app/src/main/java/com/example/a0002_app_messagequeue/MainActivity.java
@@ -11,6 +11,41 @@ public class MainActivity extends AppCompatActivity {
     private Button mButton;
     private final String TAG="MessageTest";
     private int ButtonCnt = 0;
+    private Thread myThread;
+    private MyThread myThread2;
+    //缺点：无法扩展更多的线程函数
+    class MyRunable implements Runnable {
+        @Override
+        public void run() {
+            int count = 0;
+            for (;;) {
+                Log.d(TAG, "MyThread "+count);
+                count++;
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    class MyThread extends Thread {
+        @Override
+        public void run() {
+            super.run();
+            int count = 0;
+            for (;;) {
+                Log.d(TAG, "MyThread2 "+count);
+                count++;
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +60,11 @@ public class MainActivity extends AppCompatActivity {
                 ButtonCnt++;
             }
         });
+
+        myThread = new Thread(new MyRunable(), "MessageTestThread");
+        myThread.start();
+
+        myThread2 = new MyThread();
+        myThread2.start();
     }
 }
